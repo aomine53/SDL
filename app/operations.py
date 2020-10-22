f2c79a90806677ee9f9cd19f41178879a6c44ffc
Vedant <vedantsasane2005@gmail.com>
2020-10-23 01:25:35 +0530

    First Commit

---
+++ app/operations.py
@@ -0,0 +1,68 @@
+import mysql.connector
+
+config = {"user": 'root',
+          "password": '1234',
+          "host": '127.0.0.1',
+          "database": 'mytestdb'
+          }
+
+
+def getdevicedata():
+    slu = []
+    mydb = mysql.connector.connect(**config)
+    cursor = mydb.cursor()
+    query = 'SELECT SLU FROM devices'
+    cursor.execute(query)
+    #print(dir(cursor))
+    # slu = cursor.fetchall()
+    for SLU in cursor:
+        slu.append(SLU[0])
+    cursor.close()
+    mydb.close()
+    return slu
+
+
+def getlivedata():
+    mydb = mysql.connector.connect(**config)
+    cursor = mydb.cursor()
+    datalist = []
+    slulist = getdevicedata()
+    for slu in slulist:
+        query = "SELECT RNO,VIN,VBAT,EDT,SPDK,LAT,LNG,APPD,TP FROM $slu355000082004871 ORDER BY EDT DESC LIMIT 1"
+        cursor.execute(query)
+        datalist.append(cursor.fetchone())
+
+    # print(datalist)
+    # rno,vin,vbat,edt,spdk,lat,lng = cursor.fetchone()
+    cursor.close()
+    mydb.close()
+    # return rno,vin,vbat,edt,spdk,lat,lng
+    return datalist
+
+
+def searchdata(start, end):
+    mydb = mysql.connector.connect(**config)
+    cursor = mydb.cursor()
+    edt = []
+    vin = []
+    vbat = []
+    appd = []
+    tp = []
+    query = "SELECT EDT,VIN,VBAT,APPD,TP FROM $slu355000082004871 WHERE EDT BETWEEN %s AND %s"
+    cursor.execute(query, (start, end))
+    for EDT, VIN, VBAT, APPD, TP in cursor:
+        edt.append(EDT)
+        vin.append(VIN)
+        vbat.append(VBAT)
+        appd.append(APPD)
+        tp.append(TP)
+    cursor.close()
+    mydb.close()
+    return edt, vin, vbat, appd, tp
+
+
+# searchdata("2020-10-09 21:36:00","2020-10-09 21:37:00")
+# print(type(getdevicedata()))
+# print(getdevicedata())
+# getlivedata()
+# print(searchdata("2020-10-09 21:36:00","2020-10-09 21:37:00"))
---
+++ app/views.py
@@ -0,0 +1,110 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.template import loader
+from django.http import HttpResponse, JsonResponse
+from django import template
+from django.db import connection
+from datetime import datetime, timedelta
+import pytz
+from app.operations import searchdata, getlivedata, getdevicedata
+
+
+@login_required(login_url="/login/")
+def index(request):
+    return render(request, "index.html")
+
+
+def get_live_data(request):
+    datalist = getlivedata()
+    dataobj = []
+    for data in datalist:
+        rno, vin, vbat, edt, spdk, lat, lng, appd, tp = data
+        data1 = {"rno": rno, "vbat": vbat, "vin": vin, "spdk": spdk, "time": edt.strftime("%Y-%m-%d %H:%M:%S%z"),
+                 "lat": lat,
+                 "lng": lng, "appd": appd, "tp": tp}
+        dataobj.append(data1)
+
+    cont = {"data": dataobj}
+    return JsonResponse(cont)
+
+
+def get_archive_data(request):
+    context = {}
+    utc = pytz.UTC
+
+    if request.method == "POST":
+        newtime = []
+        newVin = []
+        newVbat = []
+        newAppt = []
+        newTp = []
+        fromData = (datetime.strptime(request.POST["from"], '%Y-%m-%d %H:%M:%S'))
+        toData = (datetime.strptime(request.POST["to"], '%Y-%m-%d %H:%M:%S'))
+        edt, vin, vbat, appt, tp = searchdata(fromData, toData)
+        for i in range(0, len(edt) - 2):
+            # if appt[i] is None or tp[i] is None:
+            #     appt[i] = "None"
+            #     tp[i] = "None"
+            if fromData <= edt[i] <= toData:
+                newtime.append(edt[i].strftime('%Y-%m-%d %H:%M:%S%z'))
+                newVin.append(vin[i])
+                newVbat.append(vbat[i])
+                newAppt.append(appt[i])
+                newTp.append(tp[i])
+                if (edt[i + 1] - edt[i]) > timedelta(seconds=2):
+                    difference = int(edt[i + 1].timestamp() - edt[i].timestamp())
+                    # print(difference)
+                    for sec in range(1, difference):
+                        temptimedate = edt[i] + timedelta(seconds=2)
+                        newtime.append(temptimedate.strftime('%Y-%m-%d %H:%M:%S%z'))
+                        newVin.append(None)
+                        newVbat.append(None)
+                        newAppt.append(None)
+                        newTp.append(None)
+
+            # while True:
+            #     if(fromData > toData):
+            #         break
+            #     elif(fromData == i.edt):
+            #         newtime.append(i.edt.strftime("%H:%M:%S"))
+            #         newVin.append(i.vin)
+            #     else:
+            #         newtime.append(fromData.strftime("%H:%M:%S"))
+            #         newVin.append("s")
+            #         fromData += timedelta(seconds=1)
+
+        context = {"fromData": fromData, "toData": toData, "labels": newtime, "Vin": newVin,
+                   "Vbat": newVbat, "Appd": newAppt, "Tp": newTp}
+        # print(context.get("Vin"))
+        return JsonResponse(context)
+    else:
+        html_template = loader.get_template('page-404.html')
+        return HttpResponse(html_template.render(context, request))
+
+
+@login_required(login_url="/login/")
+def pages(request):
+    context = {"1":[1,2,3,4,5,6,7,8,9,10]}
+    # All resource paths end in .html.
+    # Pick out the html file name from the url. And load that template.
+    try:
+
+        load_template = request.path.split('/')[-1]
+        html_template = loader.get_template(load_template)
+
+        return HttpResponse(html_template.render(context, request))
+
+    except template.TemplateDoesNotExist:
+
+        html_template = loader.get_template('page-404.html')
+        return HttpResponse(html_template.render(context, request))
+
+    except:
+
+        html_template = loader.get_template('page-500.html')
+        return HttpResponse(html_template.render(context, request))
